0f6b98fe9265b37c77f49f2f00b56210c27ea044
Creation Duwal <creationduwal@gmail.com>
2024-10-17 17:21:55 +0545

    chore: fix image name capitalization and fix slug format

--- scripts/contentmap.js
+++ scripts/contentmap.js
@@ -4,14 +4,14 @@ const contentFiles = {
     file: "contributing.md",
     image: "./assets/contributing illustration.png",
   },
-  "besttutorial": {
+  "best-youtuber-to-learn-python-and-javascript": {
     title: "Best Youtuber to learn Python and JavaScript",
     file: "tutorial.md",
-    image: "./assets/tutorial-assets/js-yt.png"
+    image: "./assets/tutorial-assets/js-yt.png",
   },
-  "Strategies for Balancing Chemical Equations":{
+  "strategies-for-balancing-chemical-equations": {
     title: "Strategies for Balancing Chemical Equations ",
     file: "Strategies-for-Balancing-Chemical-Equations.md",
-    image: "./assets/chemical Balancing.png",
+    image: "./assets/Chemical Balancing.png",
   },
 };
